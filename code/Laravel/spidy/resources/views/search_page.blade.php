334c74a34b85b468123c6ae4f7c5dba3929bbd01
ayushpratap <ayushs56@gmail.com>
2017-07-31 00:13:01 +0530

    Chnage name of the folder

--- code/Laravel/spidy/resources/views/search_page.blade.php
+++ code/Laravel/spidy/resources/views/search_page.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1>	Search Page	</h1>
-<form>
+<form action="#" method="GET">
 <input type="text" name="search_query" id="search_query">
 <input type="submit" name="submit_search_query" id="submit_search_query">
 </form>
